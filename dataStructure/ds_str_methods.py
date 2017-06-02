b3b5e55161547b6aa09883c94da63701de367bbc
baikangwang <baikangwang@hotmail.com>
2017-06-02 16:27:34 +0800

    Data Structures

---
+++ dataStructure/ds_str_methods.py
@@ -0,0 +1,16 @@
+name="Swaroop"
+
+if name.startswith("Swa"):
+    print("Yes, the string starts with 'Swa'")
+
+if "roop" in name:
+    print("Yes, it contains the string 'roop'")
+
+if name.find("war")!=-1:
+    print("Yes, it contains the string 'war'")
+
+delimiter="_*_"
+
+mylist=['Brazil',"Russia","India","China"]
+print(delimiter.join(mylist))
+
